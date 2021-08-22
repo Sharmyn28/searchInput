bc8ef56bc1ca3e14ea757aeeb2817de65e6a19e6
Milagros <sharmynrodriguez@gmail.com>
2021-08-22 02:48:55 -0400

    feat: initial input

---
+++ src/App.js
@@ -0,0 +1,34 @@
+import React from 'react'
+import { DataQuery } from '@dhis2/app-runtime'
+import i18n from '@dhis2/d2-i18n'
+import ItemSelector from './components/visualizationSearch/ItemSelector'
+import classes from './App.module.css'
+
+const query = {
+    me: {
+        resource: 'me',
+    },
+}
+
+const MyApp = () => (
+    <div className={classes.wrapper}>
+        <DataQuery query={query}>
+            {({ error, loading, data }) => {
+                if (error) return <span>ERROR</span>
+                if (loading) return <span>...</span>
+                return (
+                    <>
+                        <h1>
+                            {i18n.t('Hello {{name}}', { name: data.me.name })}
+                        </h1>
+                        <h3>{i18n.t('Welcome to DHIS2!')}</h3>
+                    </>
+                )
+            }}
+        </DataQuery>
+
+        <ItemSelector/>
+    </div>
+)
+
+export default MyApp
